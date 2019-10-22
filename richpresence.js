90a0e0023fea73efa5d597756954f43e340b4955
Cody Swendrowski <cody@ironmoosedevelopment.com>
2019-10-21 22:00:57 -0500

    Actor data output

--- richpresence.js
+++ richpresence.js
@@ -12,18 +12,35 @@
       return;
     }
     window.DiscordRichPresence.loaded = version;
+
+    function getCurrentSceneName() {
+      var scenes = game.scenes.entities;
+      var firstActiveScene = scenes.find(function(element) { return element.active; });
+
+      if (!firstActiveScene) return "Unknown";
+
+      return firstActiveScene.name;
+    }
+
+    function getCurrentActorName() {
+      return game.actors.entities[1].token.name;
+    }
+
+    function getCurrentActorHealth() {
+      return game.actors.entities[1].data.health.value;
+    }
+
+    function getCurrentActorMaxHealth() {
+      return game.actors.entities[1].data.health.max;
+    }
   
     window.DiscordRichPresence.setup = () => {
       console.log(`Discord Rich Presence | Initializing v${version}`);
   
       Hooks.on('ready', () => {
         console.log("Forge initited!");
-
-        var scenes = game.scenes.entities;
-        var firstActiveScene = scenes.find(function(element) { return element.active; });
-        var sceneName = firstActiveScene.name;
-
-        console.log("Player is currently on scene " + sceneName);
+        console.log("Player is currently on scene " + getCurrentSceneName());
+        console.log("Player is currently playing Actor " + getCurrentActorName() + " which has health " + getCurrentActorHealth() + " / " + getCurrentActorMaxHealth());
       });
     };
   })();
