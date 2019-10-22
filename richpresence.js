472f7adfa2213e6d278a9589c062ebda2b0a3047
Cody Swendrowski <cody@ironmoosedevelopment.com>
2019-10-21 20:00:56 -0500

    The most basic module possible

---
+++ richpresence.js
@@ -0,0 +1,24 @@
+(() => {
+    const version = 1;  //Current Version
+  
+    //Bootstrap
+    if (!window.DiscordRichPresence) {
+      window.DiscordRichPresence = {loaded: 0};
+      window.DiscordRichPresence.setup = () => console.error('Discord Rich Presence | Failed to setup Discord Rich Presence');
+      $(() => window.DiscordRichPresence.setup());
+    }
+  
+    if (window.DiscordRichPresence.loaded >= version) {
+      return;
+    }
+    window.DiscordRichPresence.loaded = version;
+  
+    window.DiscordRichPresence.setup = () => {
+      console.log(`Discord Rich Presence | Initializing v${version}`);
+  
+      Hooks.on('init', () => {
+        console.log("Forge initited!");
+      });
+    };
+  })();
+  
